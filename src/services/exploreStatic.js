4a82a8c380dbe8306fd93c27b178713cc9f8740b
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-05-01 14:19:02 -0300

    Soma changes

--- src/services/exploreStatic.js
+++ src/services/exploreStatic.js
@@ -10,14 +10,14 @@ export default [
     id: 2,
     name: 'comprascleiton',
     description: 'O lugar do seu pé!',
-    address: 'Campina Grande, PB',
+    address: 'Sumé, PB',
     avatar: 'https://avatars0.githubusercontent.com/u/4669899?s=50&v=4',
   },
   {
     id: 3,
     name: 'lojasmaia',
     description: 'Essa é uma boa descrição!',
-    address: 'Campina Grande, PB',
+    address: 'Boa Vista, PB',
     avatar: 'https://avatars2.githubusercontent.com/u/16545335?s=50&v=4',
   },
   {
@@ -31,14 +31,14 @@ export default [
     id: 5,
     name: 'comprascleiton',
     description: 'O lugar do seu pé!',
-    address: 'Campina Grande, PB',
+    address: 'Sumé, PB',
     avatar: 'https://avatars0.githubusercontent.com/u/4669899?s=50&v=4',
   },
   {
     id: 6,
     name: 'lojasmaia',
     description: 'Essa é uma boa descrição!',
-    address: 'Campina Grande, PB',
+    address: 'Boa Vista, PB',
     avatar: 'https://avatars2.githubusercontent.com/u/16545335?s=50&v=4',
   },
 ];
